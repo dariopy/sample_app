597b3ee115adb9da0549f0fdb12c4de1a4d6aad1
Dario Alvarez <dariopy@gmail.com>
2011-05-07 19:26:31 -0700

    Initial commit capelu

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '7916fe6d5e7fea72bddca74bbafb4ac407662265e2fd3a556e2f76779745deec0d161eb96528eee1d30a1723ec7f61d96b3d7ac020a082f9712c58f871b48c53'
